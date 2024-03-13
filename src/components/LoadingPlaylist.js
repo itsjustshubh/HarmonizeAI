8cc98ca995f387fdd59d42cadbcc5f4b15b6bad0
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-03-13 03:37:42 -0400

    Better Playlist UI/UX

--- src/components/LoadingPlaylist.js
+++ src/components/LoadingPlaylist.js
@@ -8,8 +8,7 @@ const LoadingPlaylist = () => {
     const [finalResults, setFinalResults] = useState(null);
     const [additionalTracks, setAdditionalTracks] = useState([]);
     const [showErrorPopup, setShowErrorPopup] = useState(false);
-    const { tracks, setTracks } = useContext(TrackContext);
-
+    const { tracks } = useContext(TrackContext);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -36,33 +35,10 @@ const LoadingPlaylist = () => {
     const renderDetails = (details) => {
         return Object.entries(details).map(([key, value], index) => (
             <div key={index} className="text-lg font-medium text-gray-700 dark:text-gray-400">
-                <span className="capitalize">{key.replace(/_/g, ' ')}:</span> {value.toString()}
+                <span className="capitalize font-bold">{key.replace(/_/g, ' ')}:</span> {value.toString()}
             </div>
         ));
     };
-
-    const ErrorPopup = () => (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
-            <div className="bg-white rounded shadow-lg p-6 max-w-sm w-full">
-                <h2 className="text-xl font-bold mb-2">Recently Played Tracks</h2>
-                <p>You don't have any recently played tracks. We are signing you out, Bye!</p>
-            </div>
-        </div>
-    );
-
-    useEffect(() => {
-        if (finalResults) {
-            fetchAdditionalTracks();
-        }
-    }, [finalResults]);
-
-    useEffect(() => {
-        if (!tracks || tracks.length === 0) {
-            setShowErrorPopup(true);
-            handleLogout();
-            setTimeout(() => navigate('/spotify-login'), 3000);
-        }
-    }, [tracks, navigate]);
 
     // Function to remove duplicates from the tracks array
     const removeDuplicates = (tracks) => {
@@ -100,9 +76,7 @@ const LoadingPlaylist = () => {
             });
         });
 
-        const tracks = removeDuplicates(relevantTracks);
-        setTracks(tracks);
-        return tracks;
+        return removeDuplicates(relevantTracks);
     };
 
     const playlistTracks = filterTracksByValence(finalResults, tracks);
@@ -117,59 +91,99 @@ const LoadingPlaylist = () => {
         return data.tracks.items;
     };
 
+    const [countdown, setCountdown] = useState(5); // starts from 5 seconds
+    const ErrorPopup = () => (
+        <div>
+            <div>
+                <h2 className="text-xl font-bold mb-2 text-white dark:text-gray-800">Recently Played Tracks</h2>
+                <p className="text-lg text-white dark:text-gray-800">As you don't have any recently played tracks. Redirecting you in {countdown}...</p>
+            </div>
+        </div>
+    );
+
+    useEffect(() => {
+        if (finalResults) {
+            fetchAdditionalTracks();
+        }
+    }, [finalResults]);
+
+    useEffect(() => {
+        if (!tracks || tracks.length === 0) {
+            setShowErrorPopup(true);
+            handleLogout();
+            const interval = setInterval(() => {
+                setCountdown((prevCountdown) => prevCountdown - 1);
+            }, 1000);
+
+            setTimeout(() => {
+                clearInterval(interval);
+                navigate('/spotify-login');
+            }, 5000); // 5 seconds
+
+            return () => clearInterval(interval);
+        }
+    }, [tracks, navigate]);
+
     return (
         <div>
-            {!showErrorPopup &&
+            {showErrorPopup ? (
+                    <div className="message-box bg-gray-800 dark:bg-white p-6 rounded-3xl shadow-lg max-w-lg w-full">
+                        <ErrorPopup/>
+                    </div>
+                ) :
                 <div className="message-box bg-white dark:bg-gray-800 p-6 rounded-3xl shadow-lg max-w-lg w-full">
-                    {latestMessage && (
+                    {latestMessage ? (
+                            <div className="animate-fade-in-down">
+                                <p className="text-2xl text-gray-700 dark:text-gray-300 font-semibold">{latestMessage.stage}</p>
+                                {latestMessage.details &&
+                                    <div className="space-y-2 mt-3 text-gray-700 dark:text-gray-300">{renderDetails(latestMessage.details)}</div>}
+                            </div>
+                        ) :
                         <div className="animate-fade-in-down">
-                            <p className="text-2xl text-gray-700 dark:text-gray-300 font-semibold">{latestMessage.stage}</p>
-                            {latestMessage.details &&
-                                <div className="space-y-2 mt-3">{renderDetails(latestMessage.details)}</div>}
+                            <p className="text-2xl text-gray-700 dark:text-gray-300 font-semibold">Waiting To Send Response</p>
+                            <div className="space-y-2 mt-3 text-gray-700 dark:text-gray-300">The response has not been sent or received yet. I would suggest reloading the login page.</div>
                         </div>
-                    )}
+                    }
 
                     {finalResults && (
                         <div>
-                            <div className="animate-fade-in-down mt-4">
-                                <p className="text-2xl text-gray-700 dark:text-gray-300 font-semibold">Analysis Results:</p>
-                                <div className="space-y-2 mt-3 text-lg font-medium text-gray-700 dark:text-gray-400">
-                                    {JSON.stringify(finalResults, null, 2)}
+                        <div className="animate-fade-in-down mt-4">
+                            <p className="text-2xl text-gray-700 dark:text-gray-300 font-semibold">Analysis Results:</p>
+                            <div className="space-y-2 mt-3 text-lg font-medium text-gray-700 dark:text-gray-400">
+                                {JSON.stringify(finalResults, null, 2)}
+                            </div>
+                        </div>
+
+                        <div className="animate-fade-in-down mt-4">
+                            <div className="mt-4">
+                                <h3 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-white">Playlist
+                                    Tracks</h3>
+                                <div className="space-y-4">
+                                    {playlistTracks.map((track, index) => (
+                                        <div key={index}
+                                             className="flex items-center bg-white dark:bg-gray-800 p-4 rounded-lg shadow">
+                                            <img src={track.albumImageUrl} alt={track.name}
+                                                 className="w-12 h-12 object-cover rounded-full mr-4"/>
+                                            <div>
+                                                <p className="text-lg font-medium text-gray-700 dark:text-gray-200">{track.name}</p>
+                                                <p className="text-sm text-gray-500 dark:text-gray-400">{track.artist}</p>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
 
-                            <div className="animate-fade-in-down mt-4">
-                                <div className="mt-4">
-                                    <h3 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-white">Playlist
-                                        Tracks</h3>
-                                    <div className="space-y-4">
-                                        {/*{playlistTracks.map((track, index) => (*/}
-                                        {/*    <div key={index}*/}
-                                        {/*         className="flex items-center bg-white dark:bg-gray-800 p-4 rounded-lg shadow">*/}
-                                        {/*        <img src={track.albumImageUrl} alt={track.name}*/}
-                                        {/*             className="w-12 h-12 object-cover rounded-full mr-4"/>*/}
-                                        {/*        <div>*/}
-                                        {/*            <p className="text-lg font-medium text-gray-700 dark:text-gray-200">{track.name}</p>*/}
-                                        {/*            <p className="text-sm text-gray-500 dark:text-gray-400">{track.artist}</p>*/}
-                                        {/*        </div>*/}
-                                        {/*    </div>*/}
-                                        {/*))}*/}
-                                    </div>
-                                </div>
-
-                                <button
-                                    onClick={() => navigate('/playlist')}
-                                    className="py-2 px-6 bg-indigo-600 text-white w-full font-bold rounded-lg shadow-md hover:bg-indigo-700 mt-6"
-                                >
-                                    View Playlist
-                                </button>
-                            </div>
+                            <button
+                                onClick={() => navigate('/playlist')}
+                                className="py-2 px-6 bg-indigo-600 text-white w-full font-bold rounded-lg shadow-md hover:bg-indigo-700 mt-6"
+                            >
+                                View Playlist
+                            </button>
+                        </div>
                         </div>
                     )}
                 </div>
             }
-
-            {showErrorPopup && <ErrorPopup/>}
         </div>
     );
 };
--- src/components/PlaylistDisplay.js
+++ src/components/PlaylistDisplay.js
@@ -1,11 +1,25 @@
 import React, { useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { TrackContext } from './TrackContext';
 
 const PlaylistDisplay = () => {
-    console.log("PlaylistDisplay rendered");
     const navigate = useNavigate();
+    const location = useLocation();
+
     const { tracks } = useContext(TrackContext);
+    const playlistId = location.state?.playlistId; // Retrieve the playlist ID passed from SpotifyLogin
+
+    const navigateToOutro = () => {
+        navigate('/outro');
+    };
+
+    // Optional: Function to open the playlist in Spotify
+    const openPlaylistInSpotify = () => {
+        if (playlistId) {
+            const url = `https://open.spotify.com/playlist/${playlistId}`;
+            window.open(url, '_blank');
+        }
+    };
 
     // Function to handle logout
     const handleLogout = () => {
@@ -20,41 +34,33 @@ const PlaylistDisplay = () => {
         }
     }, [tracks, navigate]);
 
-    const navigateToOutro = () => {
-        navigate('/outro');
-    };
-
     return (
         <div className="py-8 px-4">
             <h2 className="text-3xl font-bold mb-6 text-center text-gray-800 dark:text-gray-100">Your Playlist:</h2>
 
-            {/*<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">*/}
-            {/*    {tracks.map((track, index) => (*/}
-            {/*        <div key={index}*/}
-            {/*             className="bg-white dark:bg-gray-800 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 overflow-hidden">*/}
-            {/*            <img src={track.albumImageUrl} alt={track.name} className="w-full h-64 object-cover"/>*/}
-            {/*            <div className="p-4">*/}
-            {/*                <h3 className="text-xl font-semibold text-gray-800 dark:text-white">{track.name}</h3>*/}
-            {/*                <p className="text-sm text-gray-600 dark:text-gray-300">Artist: {track.artist}</p>*/}
-            {/*                <p className="text-sm text-gray-600 dark:text-gray-300">Valence: {parseFloat(track.valence).toFixed(2)}</p>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    ))}*/}
-            {/*</div>*/}
+            {playlistId && (
+                <div className="text-center mb-6">
+                    <button
+                        onClick={openPlaylistInSpotify}
+                        className="px-6 py-3 bg-green-600 text-white rounded-lg font-semibold text-lg hover:bg-green-700 transition duration-300 shadow-lg"
+                    >
+                        Open Playlist in Spotify
+                    </button>
+                </div>
+            )}
 
-            {/*<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">*/}
-            {/*    {tracks.map((track, index) => (*/}
-            {/*        <div key={index}*/}
-            {/*             className="bg-white dark:bg-gray-800 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 overflow-hidden">*/}
-            {/*            <img src={track.albumImageUrl} alt={track.name} className="w-full h-64 object-cover"/>*/}
-            {/*            <div className="p-4">*/}
-            {/*                <h3 className="text-xl font-semibold text-gray-800 dark:text-white">{track.name}</h3>*/}
-            {/*                <p className="text-sm text-gray-600 dark:text-gray-300">Artist: {track.artist}</p>*/}
-            {/*                <p className="text-sm text-gray-600 dark:text-gray-300">Valence: {parseFloat(track.valence).toFixed(2)}</p>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    ))}*/}
-            {/*</div>*/}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {tracks.map((track, index) => (
+                    <div key={index} className="bg-white dark:bg-gray-800 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 overflow-hidden">
+                        <img src={track.albumImageUrl} alt={track.name} className="w-full h-64 object-cover"/>
+                        <div className="p-4">
+                            <h3 className="text-xl font-semibold text-gray-800 dark:text-white">{track.name}</h3>
+                            <p className="text-sm text-gray-600 dark:text-gray-300">Artist: {track.artist}</p>
+                            <p className="text-sm text-gray-600 dark:text-gray-300">Valence: {parseFloat(track.valence).toFixed(2)}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
 
             <div className="text-center mt-10">
                 <button
